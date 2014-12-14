27cc424a2fc18417cc0236394df77f4d433cdf8f
bdo <bdo@stanford.edu>
2014-12-13 17:22:07 -0800

    trimming scripts must include collapsing

---
+++ countMappedReads.py
@@ -0,0 +1,49 @@
+# countMappedReads.py
+# 12/13/14
+
+import csv, os, sys, multiprocessing, glob
+csv.register_dialect("textdialect", delimiter='\t')
+
+def getStats(fn):
+	basename = os.path.basename(fn).split('.')[0] + '_stat.txt'
+	os.system("samtools flagstat " + fn + " > " + basename)
+	
+def main():
+	if len(sys.argv)<3: 
+		print "Usage: python countMappedReads.py <config file> <output folder>"
+		exit()
+		
+	conf = sys.argv[1]
+	ofolder = sys.argv[2]
+	if not glob.glob(ofolder + '/'): os.mkdir(ofolder)
+
+	# read list of files that need to be flagstatted
+	files = []
+	ifile = open(conf, 'r')
+	reader = csv.reader(ifile, 'textdialect')
+	for row in reader:
+		files.append(row[0])
+	ifile.close()
+	
+	# samtools
+	os.chdir(ofolder)
+	p = multiprocessing.Pool(len(files))
+	p.map(getStats, files)
+	
+	# read into output file
+	ofile = open("allstats.txt", 'w')
+	writer = csv.writer(ofile, 'textdialect')
+	
+	statfiles = glob.glob("*_stat.txt")
+	for fn in statfiles:
+		ifile = open(fn, 'r')
+		total = ifile.readline().split(' ')[0]
+		ifile.readline()
+		mapped = ifile.readline().split(' ')[0]
+		ifile.close()
+		name = fn[:-9]
+		writer.writerow([name, total, mapped, float(mapped)/float(total) * 100])
+	ofile.close()	
+	
+if __name__ == "__main__":
+	main()
--- trim.sh
+++ trim.sh
@@ -2,5 +2,6 @@ x=${1%.*}
 basename=${x##*/}
 fasta=$basename.fa
 fastq_trimmed=${basename}_trimmed.fastq
-cat $1 | fastx_trimmer -f7 -Q33 | fastx_clipper -n -l25 -Q33 -a AGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG | fastq_quality_filter -Q33 -q25 -p80  > $fastq_trimmed
+cat $1 | fastx_clipper -n -l33 -Q33 -a AGATCGGAAGAGCGGTTCAGCAGGAATGCCGAGACCGATCTCGTATGCCGTCTTCTGCTTG | fastq_quality_filter -Q33 -q25 -p80 | fastx_collapser -Q33 > $fasta
+fasta_to_fastq.pl $fasta | fastx_trimmer -f14 -Q33 > $fastq_trimmed
 rm -f $fasta
